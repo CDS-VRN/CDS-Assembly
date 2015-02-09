938464a7848773353c57d912b61b22e9b289cf2d
Stefan van Wouw <stefan.van.wouw@geodan.nl>
2015-02-09 18:15:07 +0100

    Database container can now automatically update its database. When this fails the database is shutdown and an error is displayed, not corrupting the data.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
-DOCKER_HOST="tcp://127.0.0.1:2375"
+
+if [[ $# < 2 ]]
+then
+
+    echo "Insufficient arguments provided, required: <docker host address tcp://127.0.0.1:2375> <deploy version> [<version to upgrade from if different from currently running>]"
+    exit 1
+fi
+DOCKER_HOST=$1
 DOCKER_CMD="docker -H ${DOCKER_HOST}"
-DEPLOY_VERSION="2.2"
+DEPLOY_VERSION=$2
 
 echo "Deploying version: ${DEPLOY_VERSION} ; host ${DOCKER_HOST}."
-CURRENT_VERSION=$(${DOCKER_CMD} ps -a | grep "cds-master-postgresql" | awk 'BEGIN {FS=" "}; {print $2}' | awk 'BEGIN {FS=":"}; {print $2}')
+CURRENT_VERSION=$(${DOCKER_CMD} ps -a | grep "cds-master-admin" | awk 'BEGIN {FS=" "}; {print $NF}' | awk 'BEGIN {FS="_"}; {print $2}')
 
+
+# Note that we can upgrade from a different version that the currently running in case of an error after deploying, we are already running the newer version but we want to still upgrade from the previous in order to run the fixed update scripts for the database and LDAP.
+if [[ $# > 2 ]]
+then
+    PREV_VERSION=$3
+else
+    PREV_VERSION=${CURRENT_VERSION}
+fi
+echo "Upgrading from version ${PREV_VERSION}"
+
+echo "Running containers before upgrade:"
+${DOCKER_CMD} ps -a
 
 if [[ -z "${CURRENT_VERSION}" ]]
 then
@@ -28,90 +47,107 @@ else
 
   echo "Stopping existing ${CURRENT_VERSION} containers..."
   # Stop all existing containers.
-  ${DOCKER_CMD} stop cds-master-cron
-  ${DOCKER_CMD} stop cds-master-apache
-  ${DOCKER_CMD} stop cds-master-jobexecutor
-  ${DOCKER_CMD} stop cds-master-ldap
-  ${DOCKER_CMD} stop cds-master-postgresql
+  ${DOCKER_CMD} stop cds-master-cron_${CURRENT_VERSION}
+  ${DOCKER_CMD} stop cds-master-apache_${CURRENT_VERSION}
+  ${DOCKER_CMD} stop cds-master-webservices_${CURRENT_VERSION}
+  ${DOCKER_CMD} stop cds-master-jobexecutor_${CURRENT_VERSION}
+  ${DOCKER_CMD} stop cds-master-admin_${CURRENT_VERSION}
+  ${DOCKER_CMD} stop cds-master-ldap_${CURRENT_VERSION}
+  ${DOCKER_CMD} stop cds-master-postgresql_${CURRENT_VERSION}
 
   echo "Removing existing ${CURRENT_VERSION} containers (except data only containers) ..."
   # Remove existing non-data-only containers.
-  ${DOCKER_CMD} rm cds-master-cron
-  ${DOCKER_CMD} rm cds-master-apache
-  ${DOCKER_CMD} rm cds-master-webservices
-  ${DOCKER_CMD} rm cds-master-jobexecutor
-  ${DOCKER_CMD} rm cds-master-admin
-  ${DOCKER_CMD} rm cds-master-ldap
-  ${DOCKER_CMD} rm cds-master-postgresql
-  ${DOCKER_CMD} rm cds-master-config
+
+  ${DOCKER_CMD} rm cds-master-cron_${CURRENT_VERSION}
+  ${DOCKER_CMD} rm cds-master-apache_${CURRENT_VERSION}
+  ${DOCKER_CMD} rm cds-master-webservices_${CURRENT_VERSION}
+  ${DOCKER_CMD} rm cds-master-jobexecutor_${CURRENT_VERSION}
+  ${DOCKER_CMD} rm cds-master-admin_${CURRENT_VERSION}
+  ${DOCKER_CMD} rm cds-master-ldap_${CURRENT_VERSION}
+  ${DOCKER_CMD} rm cds-master-postgresql_${CURRENT_VERSION}
+  ${DOCKER_CMD} rm cds-master-config_${CURRENT_VERSION}
+
 fi
 
 # Start the containers. Each container can perform data container updates when needed by checking the current and deploy version.
 echo "Deploying new ${DEPLOY_VERSION} containers..."
 
 echo "Generating config from cds-config..."
-${DOCKER_CMD} run --name cds-master-config -e CDS_INSPIRE_HOST=http://vrn-services.geodan.nl cds-config:${DEPLOY_VERSION}
+${DOCKER_CMD} run --name cds-master-config_${DEPLOY_VERSION} -e CDS_INSPIRE_HOST=http://vrn-services.geodan.nl cds-config:${DEPLOY_VERSION}
 
 echo "Deploying cds-postgresql..."
-${DOCKER_CMD} run --name cds-master-postgresql -P -d --volumes-from cds-master-dbdata \
+${DOCKER_CMD} run --name cds-master-postgresql_${DEPLOY_VERSION} -P -d --volumes-from cds-master-dbdata \
     -e DEPLOY_VERSION=${DEPLOY_VERSION} \
-    -e CURRENT_VERSION=${CURRENT_VERSION} \
+    -e PREV_VERSION=${PREV_VERSION} \
     --restart=always \
     cds-postgresql:${DEPLOY_VERSION}
 
 echo "Deploying cds-ldap..."
-${DOCKER_CMD} run --name cds-master-ldap -P -d --volumes-from cds-master-ldapdata \
+${DOCKER_CMD} run --name cds-master-ldap_${DEPLOY_VERSION} -P -d --volumes-from cds-master-ldapdata \
     -e DEPLOY_VERSION=${DEPLOY_VERSION} \
-    -e CURRENT_VERSION=${CURRENT_VERSION} \
+    -e PREV_VERSION=${PREV_VERSION} \
     --restart=always \
     cds-ldap:${DEPLOY_VERSION}
 
 echo "Deploying cds-admin..."
-${DOCKER_CMD} run --name cds-master-admin -P -d --volumes-from cds-master-config \
+${DOCKER_CMD} run --name cds-master-admin_${DEPLOY_VERSION} -P -d --volumes-from cds-master-config_${DEPLOY_VERSION} \
     --volumes-from cds-master-metadata \
-    --link cds-master-postgresql:db \
-    --link cds-master-ldap:ldap \
+    --link cds-master-postgresql_${DEPLOY_VERSION}:db \
+    --link cds-master-ldap_${DEPLOY_VERSION}:ldap \
     -e DEPLOY_VERSION=${DEPLOY_VERSION} \
-    -e CURRENT_VERSION=${CURRENT_VERSION} \
+    -e PREV_VERSION=${PREV_VERSION} \
     --restart=always \
     cds-admin:${DEPLOY_VERSION}
 
 echo "Deploying cds-jobexecutor..."
-${DOCKER_CMD} run --name cds-master-jobexecutor -P -d --volumes-from cds-master-config \
+${DOCKER_CMD} run --name cds-master-jobexecutor_${DEPLOY_VERSION} -P -d --volumes-from cds-master-config_${DEPLOY_VERSION} \
     --volumes-from cds-master-metadata \
-    --link cds-master-postgresql:db \
-    --link cds-master-ldap:ldap \
+    --link cds-master-postgresql_${DEPLOY_VERSION}:db \
+    --link cds-master-ldap_${DEPLOY_VERSION}:ldap \
     -e DEPLOY_VERSION=${DEPLOY_VERSION} \
-    -e CURRENT_VERSION=${CURRENT_VERSION} \
+    -e PREV_VERSION=${PREV_VERSION} \
     --restart=always \
     cds-job-executor:${DEPLOY_VERSION}
 
 echo "Deploying cds-webservices..."
-${DOCKER_CMD} run --name cds-master-webservices -P -d --volumes-from cds-master-config \
+${DOCKER_CMD} run --name cds-master-webservices_${DEPLOY_VERSION} -P -d --volumes-from cds-master-config_${DEPLOY_VERSION} \
     --volumes-from cds-master-workspaces \
-    --link cds-master-postgresql:db \
+    --link cds-master-postgresql_${DEPLOY_VERSION}:db \
     --volumes-from cds-master-metadata \
-    --link cds-master-ldap:ldap \
+    --link cds-master-ldap_${DEPLOY_VERSION}:ldap \
     -e DEPLOY_VERSION=${DEPLOY_VERSION} \
-    -e CURRENT_VERSION=${CURRENT_VERSION} \
+    -e PREV_VERSION=${PREV_VERSION} \
     --restart=always \
     cds-webservices:${DEPLOY_VERSION} 
 
 echo "Deploying cds-apache..."
-${DOCKER_CMD} run --name cds-master-apache -p 80:80 -d --link cds-master-admin:admin \
-    --link cds-master-webservices:webservices \
+${DOCKER_CMD} run --name cds-master-apache_${DEPLOY_VERSION} -p 80:80 -d --link cds-master-admin_${DEPLOY_VERSION}:admin \
+    --link cds-master-webservices_${DEPLOY_VERSION}:webservices \
     --volumes-from cds-master-metadata \
     -e CDS_SERVER_NAME=vrn.geodan.nl \
     -e CDS_WEBSERVICES_SERVER_NAME=vrn-services.geodan.nl \
     -e CDS_SERVER_ADMIN=cds-support@inspire-provincies.nl \
     -e DEPLOY_VERSION=${DEPLOY_VERSION} \
-    -e CURRENT_VERSION=${CURRENT_VERSION} \
+    -e PREV_VERSION=${PREV_VERSION} \
     --restart=always \
     cds-apache:${DEPLOY_VERSION}
 
 echo "Deploying cds-cron..."
-${DOCKER_CMD} run --name cds-master-cron -d --link cds-master-postgresql:db \
+${DOCKER_CMD} run --name cds-master-cron_${DEPLOY_VERSION} -d --link cds-master-postgresql_${DEPLOY_VERSION}:db \
     -e DEPLOY_VERSION=${DEPLOY_VERSION} \
-    -e CURRENT_VERSION=${CURRENT_VERSION} \
-    cds-cron:${DEPLOY_VERSION}
+    -e PREV_VERSION=${PREV_VERSION} \
+    cds-cron:${DEPLOY_VERSION} 
 
+
+
+# TODO: Make this more elegant.
+sleep 2
+ERROR_CHECK=$(${DOCKER_CMD} logs cds-master-postgresql_${DEPLOY_VERSION} | grep error | wc -l)
+if [[ ${ERROR_CHECK} != 0 ]]
+then
+    echo "Database update failed:"
+    exit 1
+fi
+
+echo "Existing containers after upgrade:"
+${DOCKER_CMD} ps -a
--- src/main/docker/cds-postgresql/start.sh
+++ src/main/docker/cds-postgresql/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# TODO: ADD UPDATE sequence, CREATE only now.
 
 /etc/init.d/postgresql start
 
 
 
+SUCCESS=true
 RESULT=$(psql -l | grep cds | wc -l)
-if [[ $RESULT != 1 ]]
+if [[ ${RESULT} != 1 ]]
 then
     # If no data present, create new data in external data volume (/var/lib/postgresql). This volume was already attached
     # by the time this code runs.
@@ -22,18 +22,24 @@ then
     psql -d template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;" && \
     createdb -l en_US.UTF8 -O inspire -E UTF8 -T template_postgis cds && \
     psql -d cds -f /usr/share/cds/sql/create-database.sql && \
-    psql -d cds -f /usr/share/cds/sql/add-themes.sql
-else
+    psql -d cds -f /usr/share/cds/sql/add-themes.sql || SUCCESS=false
+elif [[ ! -z "${PREV_VERSION}" && ! -z "${DEPLOY_VERSION}" && "${PREV_VERSION}" != "${DEPLOY_VERSION}" ]]
+then
     # If previous version present, and previous version does not equal version to be deployed, check for database
     # structure updates to be applied.
-    echo "Database already exists."
-    #$VERSION_PREVIOUS
-    #$VERSION_NEW
+    echo "Database already exists, and upgrade needed."
+
+    python /opt/database_update.py /usr/share/cds/sql/updates > /tmp/update_database.sql && \
+    psql -d cds -f /tmp/update_database.sql || SUCCESS=false
 
 fi
-
 /etc/init.d/postgresql stop
 
-# Always start (non-daemon/blocking) PostgreSQL process.
-/usr/lib/postgresql/9.1/bin/postgres -D /var/lib/postgresql/9.1/main \
--c config_file=/etc/postgresql/9.1/main/postgresql.conf
+# Always start (non-daemon/blocking) PostgreSQL process when SUCCESS==true
+if [[ "${SUCCESS}" == true ]]
+then
+    /usr/lib/postgresql/9.1/bin/postgres -D /var/lib/postgresql/9.1/main \
+    -c config_file=/etc/postgresql/9.1/main/postgresql.conf
+else
+    echo "Database upgrading failed, see error log for details. Please fix image and try again."
+fi
